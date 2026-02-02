ba254b818f0f1d93ea0ce4d43a2fb98acf8c251e
Dylan Prevost <prevost.dylan89@gmail.com>
2026-02-02 21:37:56 +0000

    File changes

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -206,19 +206,6 @@ export default function Collection() {
     }
   });
 
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: { staggerChildren: 0.05 }
-    }
-  };
-
-  const item = {
-    hidden: { opacity: 0, y: 20 },
-    show: { opacity: 1, y: 0 }
-  };
-
   return (
     <div className="min-h-screen">
       {/* Header */}
