6170aa43264d6e93e0911cc4120ce80801aabaa6
Dylan Prevost <prevost.dylan89@gmail.com>
2026-03-07 12:31:01 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { LanguageProvider, useLanguage } from '@/components/LanguageContext';
 import { motion, AnimatePresence } from 'framer-motion';
