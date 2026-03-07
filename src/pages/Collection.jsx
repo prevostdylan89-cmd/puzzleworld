fe60c837557810f6189dcda8590987f577540e26
Dylan Prevost <prevost.dylan89@gmail.com>
2026-03-07 13:50:24 +0000

    File changes

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
