f7270a9ca2651d0a7e815a318df7ced0abcb85fc
Dylan Prevost <prevost.dylan89@gmail.com>
2026-03-30 15:53:47 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -275,7 +275,6 @@ export default function Home() {
                 </motion.div>
               ))}
             </div>
-          )}
         </section>
 
         {/* Mobile Événements - vertical cards */}
